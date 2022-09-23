400aace708a6b3b149741605bb0502fc38361c16
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-09-23 09:09:02 +0300

    Assignment Complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,14 @@
 class StudentsController < ApplicationController
 
   def index
+    # byebug
     students = Student.all
+    # render json: params[:name] ? Student.where(first_name: params[:name]).or(Student.where(last_name: params[:name])) : students
     render json: students
+  end
+
+  def show
+    render json: Student.find(params[:id])
   end
 
 end
